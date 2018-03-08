aaf765b57d5a65a4e289d5c0c864e40fa550ef20
Alan Dixon <alan.g.dixon@gmail.com>
2018-03-08 22:44:28 +0000

    added rest-gateway-php from github, upgrader and payment processor record mgd file

---
+++ CRM/Core/Payment/Faps.mgd.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * This record will be automatically inserted, updated, or deleted from the
+ * database as appropriate. For more details, see "hook_civicrm_managed" at:
+ * http://wiki.civicrm.org/confluence/display/CRMDOC/Hook+Reference
+ */
+
+return array(
+   0 =>
+    array(
+      'name' => 'iATS Payment FAPS Processor',
+      'entity' => 'payment_processor_type',
+      'params' =>
+        array(
+          'version' => 3,
+          'title' => 'iATS Payment FAPS Credit Card',
+          'name' => 'iATS Payment FAPS Credit Card',
+          'description' => 'iATS Payment Credit Card Processor using 1stPay',
+          'user_name_label' => 'Agent Code',
+          'password_label' => 'Secret Key',
+          'signature_label' => 'Access Key',
+          'class_name' => 'Payment_Faps',
+          'url_site_default' => 'https://secure.1stpaygateway.net/secure/RestGW/Gateway/Transaction/',
+          'url_site_test_default' => 'https://secure-v.goemerchant.com/secure/RestGW/Gateway/Transaction/',
+//          'url_recur_default' => 'https://secure.1stpaygateway.net/secure/RestGW/Gateway/Transaction/Sale'
+//          'url_recur_test_default' => 'https://secure-v.goemerchant.com/secure/RestGW/Gateway/Transaction/',
+          'billing_mode' => 1,
+          'payment_type' => 1,
+          'is_recur' => 0,
+          'payment_instrument_id' => 1,
+          'is_active' => 1,
+        ),
+    )
+);
+ 
---
+++ CRM/Core/Payment/Faps.php
@@ -0,0 +1,70 @@
+<?php
+
+require_once 'CRM/Core/Payment.php';
+
+class CRM_Core_Payment_Faps extends CRM_Core_Payment {
+
+  /**
+   * mode of operation: live or test
+   *
+   * @var object
+   * @static
+   */
+  static protected $_mode = null;
+
+  /**
+   * Constructor
+   *
+   * @param string $mode the mode of operation: live or test
+   *
+   * @return void
+   */
+  function __construct( $mode, &$paymentProcessor ) {
+    $this->_mode             = $mode;
+    $this->_paymentProcessor = $paymentProcessor;
+    $this->_processorName    = ts('iATS Payments 1st American Payment System Interface');
+  }
+
+  /**
+   * This function checks to see if we have the right config values
+   *
+   * @return string the error message if any
+   * @public
+   */
+  function checkConfig( ) {
+
+    $error = array();
+
+    if (empty($this->_paymentProcessor['user_name'])) {
+      $error[] = ts('The Agent Code is not set in the Administer CiviCRM &raquo; System Settings &raquo; Payment Processors.');
+    }
+    if (empty($this->_paymentProcessor['password'])) {
+      $error[] = ts('Password is not set in the Administer CiviCRM &raquo; System Settings &raquo; Payment Processors.');
+    }
+
+    if (!empty($error)) {
+      return implode('<p>', $error);
+    }
+    else {
+      return NULL;
+    }
+  }
+
+  function doDirectPayment(&$params) {
+    CRM_Core_Error::fatal(ts('This function is not implemented'));
+  }
+
+  /**
+   * Sets appropriate parameters for checking out to UCM Payment Collection
+   *
+   * @param array $params  name value pair of contribution datat
+   *
+   * @return void
+   * @access public
+   *
+   */
+  function doTransferCheckout( &$params, $component ) {
+    CRM_Core_Error::fatal(ts('This function is not implemented'));
+  }
+
+}
