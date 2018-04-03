68a9209498f61d93882c4161109a54c94c4617c1
Alan Dixon <alan.g.dixon@gmail.com>
2018-04-03 14:27:24 +0000

    initial almost working doDirectPayment

--- CRM/Core/Payment/Faps.php
+++ CRM/Core/Payment/Faps.php
@@ -36,10 +36,10 @@ class CRM_Core_Payment_Faps extends CRM_Core_Payment {
     $error = array();
 
     if (empty($this->_paymentProcessor['user_name'])) {
-      $error[] = ts('The Agent Code is not set in the Administer CiviCRM &raquo; System Settings &raquo; Payment Processors.');
+      $error[] = ts('Processor Id is not set in the Administer CiviCRM &raquo; System Settings &raquo; Payment Processors.');
     }
-    if (empty($this->_paymentProcessor['password'])) {
-      $error[] = ts('Password is not set in the Administer CiviCRM &raquo; System Settings &raquo; Payment Processors.');
+    if (empty($this->_paymentProcessor['signature'])) {
+      $error[] = ts('Merchant Key is not set in the Administer CiviCRM &raquo; System Settings &raquo; Payment Processors.');
     }
 
     if (!empty($error)) {
@@ -48,23 +48,55 @@ class CRM_Core_Payment_Faps extends CRM_Core_Payment {
     else {
       return NULL;
     }
+    // TODO: check urls vs. what I'm expecting?
   }
 
   function doDirectPayment(&$params) {
-    // CRM_Core_Error::fatal(ts('This function is not implemented'));
-    require 'vendor/1stpaygateway/rest-gateway-php/gateway.php'
-    $sale_demo = new RestGateway();
+
+    // Check for valid currency
+    if ('USD' != $params['currencyID']) {
+      return self::error('Invalid currency selection: ' . $params['currencyID']);
+    }
+    // Use the Faps Request object for interacting with FAPS
+    require_once "CRM/Faps/Request.php";
+    $isRecur = CRM_Utils_Array::value('is_recur', $params) && $params['contributionRecurID'];
+    // $method = $isRecur ? 'create_credit_card_customer' : 'cc';
+    $options = array('action' => 'Sale');
+    if ($this->_mode == 'test') {
+      $options['test'] = 1;
+    }
+    $faps = new Faps_Request($options);
+    $request = $this->convertParams($params, $options['action']);
+    $request['ipAddress'] = (function_exists('ip_address') ? ip_address() : $_SERVER['REMOTE_ADDR']);
     $credentials = array(
-     'merchantKey' => $this->_paymentProcessor['signature'],
-     'processorId' => $this->_paymentProcessor['user_name']
+      'merchantKey' => $this->_paymentProcessor['signature'],
+      'processorId' => $this->_paymentProcessor['user_name']
     );
-    
+    // Make the request.
+    $result = $faps->request($credentials, $request);
+    $success = (!empty($result['isSuccess']));
+    if (!$isRecur) {
+      if ($success) {
+        $params['contribution_status_id'] = 1;
+        // For versions >= 4.6.6, the proper key.
+        $params['payment_status_id'] = 1;
+        $params['trxn_id'] = trim($result['authCode']) . ':' . trim($result['referenceNumber']);
+        $params['gross_amount'] = $params['amount'];
+        return $params;
+      }
+      else {
+        return self::error($result['errorMessages']);
+      }
+    } 
+    else {
+      return self::error(ts('Recurring function is not implemented'));
+    }
   }
 
   /**
-   * Sets appropriate parameters for checking out to UCM Payment Collection
+   * Todo?
    *
-   * @param array $params  name value pair of contribution datat
+   * @param array $params name value pair of contribution data
    *
    * @return void
    * @access public
@@ -72,6 +104,79 @@ class CRM_Core_Payment_Faps extends CRM_Core_Payment {
    */
   function doTransferCheckout( &$params, $component ) {
     CRM_Core_Error::fatal(ts('This function is not implemented'));
+  }
+
+  /**
+   * Convert the values in the civicrm params to the request array with keys as expected by FAPS
+   *
+   * @param array $params
+   * @param string $action
+   *
+   * @return array
+   */
+  protected function convertParams($params, $method) {
+    $request = array();
+    $convert = array(
+      'ownerEmail' => 'email',
+      'ownerStreet' => 'street_address',
+      'ownerCity' => 'city',
+      'ownerState' => 'state_province',
+      'ownerZip' => 'postal_code',
+      'ownerCountry' => 'country',
+      'orderId' => 'invoiceID',
+      'cardNum' => 'credit_card_number',
+//      'cardtype' => 'credit_card_type',
+      'cVV' => 'cvv2',
+    );
+    foreach ($convert as $r => $p) {
+      if (isset($params[$p])) {
+        $request[$r] = htmlspecialchars($params[$p]);
+      }
+    }
+    if (empty($params['email'])) {
+      if (isset($params['email-5'])) {
+        $request['ownerEmail'] = $params['email-5'];
+      }
+      elseif (isset($params['email-Primary'])) {
+        $request['ownerEmail'] = $params['email-Primary'];
+      }
+    }
+    $request['ownerName'] = $params['billing_first_name'].' '.$params['billing_last_name'];
+    $request['cardExpMonth'] = sprintf('%02d', $params['month']);
+    $request['cardExpYear'] = sprintf('%02d', $params['year'] % 100);
+    $request['transactionAmount'] = sprintf('%01.2f', CRM_Utils_Rule::cleanMoney($params['amount']));
+    // print_r($request); print_r($params); die();
+    return $request;
+  }
+
+
+  /**
+   *
+   */
+  public function &error($error = NULL) {
+    $e = CRM_Core_Error::singleton();
+    if (is_object($error)) {
+      $e->push($error->getResponseCode(),
+        0, NULL,
+        $error->getMessage()
+      );
+    }
+    elseif ($error && is_numeric($error)) {
+      $e->push($error,
+        0, NULL,
+        $this->errorString($error)
+      );
+    }
+    elseif (is_string($error)) {
+      $e->push(9002,
+        0, NULL,
+        $error
+      );
+    }
+    else {
+      $e->push(9001, 0, NULL, "Unknown System Error.");
+    }
+    return $e;
   }
 
 }
